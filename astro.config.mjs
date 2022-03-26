e7eb4c989f686148c876e3525fe2d448f3e466ce
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-03-27 03:03:10 +0700

    clean project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
-https://docs.astro.build/en/reference/configuration-reference/
+// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   buildOptions: {
     site: "https://AREA44.github.io/astro-firstweb",
