1eae428dca51d5291b9142a1ddd2075480f7ac0d
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-03-27 03:02:05 +0700

    clean project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-// https://astro.build/config
+import { defineConfig } from "astro/config";
+
+https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   buildOptions: {
-		site: 'https://AREA44.github.io/astro-firstweb',
-	},
+    site: "https://AREA44.github.io/astro-firstweb",
+  },
 });
