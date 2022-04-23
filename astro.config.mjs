d542fac137f1dc563fa93acdb6cca27722dc554c
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-04-23 20:17:20 +0700

    remove site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-  site: "https://AREA44.github.io",
   base: "/astro-firstweb",
 });
